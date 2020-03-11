8d0055ab35d7c12af387daecc42e63c5bcb5ac6e
Leonid <leonid.puchkov@gmail.com>
2020-03-11 12:37:32 +0200

    added carousel

--- js/main.js
+++ js/main.js
@@ -23,8 +23,42 @@ $("a[href^='#']").click(function () {
   return false;
 });
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+$('[data-modal=bannerpopup]').on('click', function () {
+  $('.overlay, #bannerpopup').fadeIn('slow');
+});
+$('.modal__close').on('click', function () {
+  $('.overlay, #bannerpopup').fadeOut('slow');
+});
+
+
 //carousel
-$(document).ready(function () {
+/* $(document).ready(function () {
   $('.carousel__inner').slick({
     infinite: true,
     speed: 1200,
@@ -49,4 +83,4 @@ $(document).ready(function () {
       }
     ]
   });
-});
+}); */
